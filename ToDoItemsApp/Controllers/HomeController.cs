b7da46d397fc85077b1b7d4ea74ac82e62f12c20
Elka Samber <ersamber@gmail.com>
2021-06-06 17:17:43 -0400

    Add project files.

---
+++ ToDoItemsApp/Controllers/HomeController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoItemsApp.Models;
+
+namespace ToDoItemsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+            private string _connectionString =
+                @"Data Source=.\sqlexpress;Initial Catalog=ToDoItems;Integrated Security=true;";
+
+        public IActionResult Index()
+        {
+            ToDoItemsDb db = new ToDoItemsDb(_connectionString);
+            IndexViewModel vm = new IndexViewModel()
+            {
+                IncompleteItems = db.GetIncompleteItems(),
+                Categories = db.GetCategories()
+            };
+            return View(vm);
+        }
+        public IActionResult AddItem()
+        {
+            ToDoItemsDb db = new ToDoItemsDb(_connectionString);
+            AddItemViewModel vm = new AddItemViewModel()
+            {
+                Categories = db.GetCategories()
+            };
+            return View(vm);
+        }
+        [HttpPost]
+        public IActionResult SubmitItem(Item item)
+        {
+            ToDoItemsDb db = new ToDoItemsDb(_connectionString);
+            db.AddItem(item);
+            return Redirect("/home/index");
+        }
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SubmitCategory(Category category)
+        {
+            ToDoItemsDb db = new ToDoItemsDb(_connectionString);
+            db.AddCategory(category);
+            return Redirect("/home/index");
+        }
+        public IActionResult MarkAsCompleted(int id)
+        {
+            ToDoItemsDb db = new ToDoItemsDb(_connectionString);
+            DateTime dateCompleted = DateTime.Now;
+            db.MarkItemAsCompleted(id, dateCompleted);
+            return Redirect("/home/index");
+        }
+        public IActionResult CompletedItems()
+        {
+            ToDoItemsDb db = new ToDoItemsDb(_connectionString);
+            CompletedItemsViewModel vm = new CompletedItemsViewModel()
+            {
+                CompletedItems = db.GetCompletedItems(),
+                Categories = db.GetCategories()
+            };
+            return View(vm);
+        }
+        public IActionResult Categories()
+        {
+            ToDoItemsDb db = new ToDoItemsDb(_connectionString);
+            CategoriesViewModel vm = new CategoriesViewModel()
+            {
+                Categories = db.GetCategories()
+            };
+            return View(vm);
+        }
+        public IActionResult EditCategory(int id)
+        {
+            ToDoItemsDb db = new ToDoItemsDb(_connectionString);
+            EditCategoryViewModel vm = new EditCategoryViewModel()
+            {
+                Category = db.GetCategoryById(id)
+            };
+            return View(vm);
+        }
+        [HttpPost]
+        public IActionResult SubmitEditedCategory(Category category)
+        {
+            ToDoItemsDb db = new ToDoItemsDb(_connectionString);
+            db.EditCategory(category);
+            return Redirect("/home/index");
+        }
+        public IActionResult ItemsForCategory(int id)
+        {
+            ToDoItemsDb db = new ToDoItemsDb(_connectionString);
+            GetByCategoryViewModel vm = new GetByCategoryViewModel()
+            {
+                ItemsByCategory = db.GetItemsByCategory(id),
+                Category = db.GetCategoryById(id)
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ ToDoItemsApp/Models/IndexViewModel.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoItemsApp.Models
+{
+    public class IndexViewModel
+    {
+        public List<Item> IncompleteItems { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+    public class AddItemViewModel
+    {
+        public List<Category> Categories { get; set; }
+    }
+    public class CompletedItemsViewModel
+    {
+        public List<Item> CompletedItems { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+    public class CategoriesViewModel
+    {
+        public List<Category> Categories { get; set; }
+    }
+    public class EditCategoryViewModel
+    {
+        public Category Category { get; set; }
+    }
+    public class GetByCategoryViewModel
+    {
+        public List<Item> ItemsByCategory { get; set; }
+        public Category Category { get; set; }
+    }
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DateDue { get; set; }
+        public DateTime? DateCompeted { get; set; }
+        public int CategoryId { get; set; }
+    }
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class ToDoItemsDb
+    {
+        private readonly string _connectionString;
+        public ToDoItemsDb(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public List<Item> GetIncompleteItems()
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = "SELECT * FROM Items i WHERE i.DateCompleted IS NULL";
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<Item> items = new List<Item>();
+                while (reader.Read())
+                {
+                    items.Add(new Item
+                    {
+                        Id = (int)reader["Id"],
+                        Title = (string)reader["Title"],
+                        DateDue = (DateTime)reader["DateDue"],
+                        CategoryId = (int)reader["CategoryId"]
+                    });
+                }
+                return items;
+            }
+        }
+        public List<Category> GetCategories()
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = "SELECT * FROM Categories c";
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<Category> categories = new List<Category>();
+                while (reader.Read())
+                {
+                    categories.Add(new Category
+                    {
+                        Id = (int)reader["Id"],
+                        Name = (string)reader["Name"]
+                    });
+                }
+                return categories;
+            }
+        }
+        public void AddCategory(Category c)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = "INSERT INTO Categories VALUES (@name)";
+                cmd.Parameters.AddWithValue("@name", c.Name);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void AddItem(Item i)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = "INSERT INTO Items (Title, CategoryId, DateDue) VALUES (@title, @categoryId, @dateDue)";
+                cmd.Parameters.AddWithValue("@title", i.Title);
+                cmd.Parameters.AddWithValue("@categoryId", i.CategoryId);
+                cmd.Parameters.AddWithValue("@dateDue", i.DateDue);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void MarkItemAsCompleted(int id, DateTime dateCompleted)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = "UPDATE Items SET DateCompleted = @dateCompleted WHERE Id = @id";
+                cmd.Parameters.AddWithValue("@dateCompleted", dateCompleted);
+                cmd.Parameters.AddWithValue("@Id", id);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public List<Item> GetCompletedItems()
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = "SELECT * FROM Items i WHERE i.DateCompleted IS NOT NULL";
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<Item> items = new List<Item>();
+                while (reader.Read())
+                {
+                    items.Add(new Item
+                    {
+                        Id = (int)reader["Id"],
+                        Title = (string)reader["Title"],
+                        DateDue = (DateTime)reader["DateDue"],
+                        DateCompeted = (DateTime)reader["DateCompleted"],
+                        CategoryId = (int)reader["CategoryId"]
+                    });
+                }
+                return items;
+            }
+        }
+        public Category GetCategoryById(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = "SELECT * FROM Categories c WHERE c.Id = @id";
+                cmd.Parameters.AddWithValue("@id", id);
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                Category category = new Category();
+                while (reader.Read())
+                {
+                    category.Name = (string)reader["Name"];
+                    category.Id = (int)reader["Id"];
+                }
+                return category;
+            }
+        }
+        public void EditCategory(Category c)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = "UPDATE Categories SET Name = @name WHERE Id = @id";
+                cmd.Parameters.AddWithValue("@name", c.Name);
+                cmd.Parameters.AddWithValue("@id", c.Id);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public List<Item> GetItemsByCategory(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            using (SqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = "SELECT * FROM Items i WHERE i.CategoryId = @id";
+                cmd.Parameters.AddWithValue("@id", id);
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<Item> items = new List<Item>();
+                while (reader.Read())
+                {
+                    items.Add(new Item
+                    {
+                        Id = (int)reader["Id"],
+                        Title = (string)reader["Title"],
+                        DateDue = (DateTime)reader["DateDue"],
+                        DateCompeted = reader.GetOrNull<DateTime?>("DateCompleted"),
+                        CategoryId = (int)reader["CategoryId"]
+                    });
+                }
+                return items;
+            }
+        }
+    }
+    public static class Extensions
+    {
+        public static T GetOrNull<T>(this SqlDataReader reader, string column)
+        {
+            object value = reader[column];
+            if (value == DBNull.Value)
+            {
+                return default(T);
+            }
+            return (T)value;
+        }
+    }
+}
